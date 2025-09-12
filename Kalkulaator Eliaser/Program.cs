95e71e731fdd8a701a8b16b730e6d74c9b90b070
sandraeliaser07-bit <sandraeliaser07@gmail.com>
2025-09-12 15:08:23 +0300

    Lisasin astendamise ja vooskeemi

---
+++ Kalkulaator Eliaser/Program.cs
@@ -0,0 +1,34 @@
+﻿int arv1 = 0;
+int arv2 = 0;
+Console.WriteLine("Sisesta arv 1");
+arv1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Sisesta arv 2");
+arv2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Millist tehet soovite teha? + - / * ^");
+string tehtetüüp = Console.ReadLine();
+float tulemus = 0;
+if (tehtetüüp == "+")
+{
+    tulemus = arv1 + arv2;
+}
+if (tehtetüüp == "-")
+{
+    tulemus = arv1 - arv2;
+}
+if (tehtetüüp == "/")
+{
+    tulemus = arv1 / arv2;
+
+}
+if (tehtetüüp == "*")
+{
+    tulemus = arv1 * arv2;
+
+}
+if (tehtetüüp == "^")
+{
+    tulemus = (float)Math.Pow(arv1, arv2);
+}
+Console.WriteLine(tulemus);
+
+
