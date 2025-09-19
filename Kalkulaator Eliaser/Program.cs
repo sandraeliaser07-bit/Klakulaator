40830cc1e0cfa52d6f6eebc7053c6edf64932ea5
sandraeliaser07 <sandraeliaser07@gmail.com>
2025-09-19 14:53:47 +0300

    Ülesande 2 joonis

--- Kalkulaator Eliaser/Program.cs
+++ Kalkulaator Eliaser/Program.cs
@@ -1,4 +1,6 @@
-﻿int arv1 = 0;
+﻿using System.ComponentModel.Design;
+
+int arv1 = 0;
 int arv2 = 0;
 Console.WriteLine("Sisesta arv 1");
 arv1 = int.Parse(Console.ReadLine());
@@ -30,5 +32,44 @@ if (tehtetüüp == "^")
     tulemus = (float)Math.Pow(arv1, arv2);
 }
 Console.WriteLine(tulemus);
-
-
+string lemmikpuuvili = "";
+string valik = "";
+string lemmikjuurvili ="";
+Console.WriteLine("Kas sulle meeldivad puuviljad või juurviljad? Kirjuta juurviljad, 0 kui kui puuvili, 1 kui juurvili");
+valik = Console.ReadLine();
+if (valik == "puuviljad")
+{
+    lemmikpuuvili = Console.ReadLine();
+    if (lemmikpuuvili == "maasikas")
+    {
+        Console.WriteLine("Maasikas ei ole puuvili");
+    }
+    else if (lemmikpuuvili == "pirn")
+    {
+        Console.WriteLine("Kas hapu pirn või magus pirn, mõlemad on head");
+    }
+    else if (lemmikpuuvili == "kartul")
+    {
+        Console.WriteLine("See ei kasva isegi puu otsas");
+    }
+    else
+    {
+        Console.WriteLine("Ei tunne seda puuvilja");
+    }
+}
+else if (valik == ("juurviljad"))
+{
+    lemmikjuurvili = Console.ReadLine();
+    if (lemmikjuurvili == "tomat")
+    {
+        Console.WriteLine("Tomat ei ole teoreetiliselt juurvili");
+    }
+}
+else if (lemmikjuurvili == "kurk")
+{
+    Console.WriteLine("Hapukurk või tavaline?");
+}
+else
+{
+     Console.WriteLine("Ei tunne seda juurvilja");
+}
