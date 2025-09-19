8b5e882a9cde4596247e722af5f2bbfea564303f
sandraeliaser07 <sandraeliaser07@gmail.com>
2025-09-19 15:05:04 +0300

    Pindala kalkulaator

---
+++ Kalkulaator Eliaser/Class1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalkulaator_Eliaser
+{
+    internal class Class1
+    {
+//        using System.ComponentModel.Design;
+
+//int arv1 = 0;
+//    int arv2 = 0;
+//    Console.WriteLine("Sisesta arv 1");
+//arv1 = int.Parse(Console.ReadLine());
+//    Console.WriteLine("Sisesta arv 2");
+//arv2 = int.Parse(Console.ReadLine());
+//    Console.WriteLine("Millist tehet soovite teha? + - / * ^");
+//string tehtetüüp = Console.ReadLine();
+//    float tulemus = 0;
+//if (tehtetüüp == "+")
+//{
+//    tulemus = arv1 + arv2;
+//}
+//if (tehtetüüp == "-")
+//{
+//    tulemus = arv1 - arv2;
+//}
+//if (tehtetüüp == "/")
+//{
+//    tulemus = arv1 / arv2;
+
+//}
+//if (tehtetüüp == "*")
+//{
+//    tulemus = arv1 * arv2;
+
+//}
+//if (tehtetüüp == "^")
+//{
+//    tulemus = (float)Math.Pow(arv1, arv2);
+//}
+//Console.WriteLine(tulemus);
+//string lemmikpuuvili = "";
+//string valik = "";
+//string lemmikjuurvili = "";
+//Console.WriteLine("Kas sulle meeldivad puuviljad või juurviljad? Kirjuta juurviljad, 0 kui kui puuvili, 1 kui juurvili");
+//valik = Console.ReadLine();
+//if (valik == "puuviljad")
+//{
+//    lemmikpuuvili = Console.ReadLine();
+//    if (lemmikpuuvili == "maasikas")
+//    {
+//        Console.WriteLine("Maasikas ei ole puuvili");
+//    }
+//    else if (lemmikpuuvili == "pirn")
+//    {
+//        Console.WriteLine("Kas hapu pirn või magus pirn, mõlemad on head");
+//    }
+//    else if (lemmikpuuvili == "kartul")
+//    {
+//        Console.WriteLine("See ei kasva isegi puu otsas");
+//    }
+//    else
+//    {
+//        Console.WriteLine("Ei tunne seda puuvilja");
+//    }
+//}
+//else if (valik == ("juurviljad"))
+//{
+//    lemmikjuurvili = Console.ReadLine();
+//    if (lemmikjuurvili == "tomat")
+//    {
+//        Console.WriteLine("Tomat ei ole teoreetiliselt juurvili");
+//    }
+//}
+//else if (lemmikjuurvili == "kurk")
+//{
+//    Console.WriteLine("Hapukurk või tavaline?");
+//}
+//else
+//{
+//    Console.WriteLine("Ei tunne seda juurvilja");
+//}
+    }
+}
--- Kalkulaator Eliaser/Program.cs
+++ Kalkulaator Eliaser/Program.cs
@@ -1,75 +1,9 @@
-﻿using System.ComponentModel.Design;
-
-int arv1 = 0;
-int arv2 = 0;
-Console.WriteLine("Sisesta arv 1");
-arv1 = int.Parse(Console.ReadLine());
-Console.WriteLine("Sisesta arv 2");
-arv2 = int.Parse(Console.ReadLine());
-Console.WriteLine("Millist tehet soovite teha? + - / * ^");
-string tehtetüüp = Console.ReadLine();
-float tulemus = 0;
-if (tehtetüüp == "+")
-{
-    tulemus = arv1 + arv2;
-}
-if (tehtetüüp == "-")
-{
-    tulemus = arv1 - arv2;
-}
-if (tehtetüüp == "/")
-{
-    tulemus = arv1 / arv2;
-
-}
-if (tehtetüüp == "*")
-{
-    tulemus = arv1 * arv2;
-
-}
-if (tehtetüüp == "^")
-{
-    tulemus = (float)Math.Pow(arv1, arv2);
-}
-Console.WriteLine(tulemus);
-string lemmikpuuvili = "";
-string valik = "";
-string lemmikjuurvili ="";
-Console.WriteLine("Kas sulle meeldivad puuviljad või juurviljad? Kirjuta juurviljad, 0 kui kui puuvili, 1 kui juurvili");
-valik = Console.ReadLine();
-if (valik == "puuviljad")
-{
-    lemmikpuuvili = Console.ReadLine();
-    if (lemmikpuuvili == "maasikas")
-    {
-        Console.WriteLine("Maasikas ei ole puuvili");
-    }
-    else if (lemmikpuuvili == "pirn")
-    {
-        Console.WriteLine("Kas hapu pirn või magus pirn, mõlemad on head");
-    }
-    else if (lemmikpuuvili == "kartul")
-    {
-        Console.WriteLine("See ei kasva isegi puu otsas");
-    }
-    else
-    {
-        Console.WriteLine("Ei tunne seda puuvilja");
-    }
-}
-else if (valik == ("juurviljad"))
-{
-    lemmikjuurvili = Console.ReadLine();
-    if (lemmikjuurvili == "tomat")
-    {
-        Console.WriteLine("Tomat ei ole teoreetiliselt juurvili");
-    }
-}
-else if (lemmikjuurvili == "kurk")
-{
-    Console.WriteLine("Hapukurk või tavaline?");
-}
-else
-{
-     Console.WriteLine("Ei tunne seda juurvilja");
-}
+﻿int a_külg = 0;
+int b_külg = 0;
+int c_külg = 0;
+int läbimõõt = 0;
+int kolmnurk = 0;
+int ristkülik = 0;
+int ring = 0;
+Console.WriteLine("Kas sinu tuba on kolmnurga, ristküliku või ringi kujuline?");
+kolmnurk = int.Parse(Console.ReadLine());
